edaa0107e5c623d8ada0c7ce00c8b1ed3c52be7b
Efe Bütüner <efebut@gmail.com>
2020-10-31 20:01:00 +0300

    v1

--- PalindromOdev/Program.cs
+++ PalindromOdev/Program.cs
@@ -10,6 +10,34 @@ namespace PalindromOdev
     {
         static void Main(string[] args)
         {
+            string kelime;
+            do
+            {
+                Console.Write("En az 3 harften oluşan bir kelime giriniz: ");
+                kelime = Console.ReadLine();
+                if (kelime.Length > 2)
+                {
+                    if (kelime != "q")
+                    {
+                        string palindrom = "";
+                        for (int i = kelime.Length - 1; i >= 0; i--)
+                        {
+                            palindrom += kelime[i];
+                        }
+                        if (palindrom == kelime)
+                        {
+                            Console.WriteLine("Girdiğiniz kelime palindromdur.");
+                        }
+                        else if (palindrom != kelime)
+                        {
+                            Console.WriteLine("Girdiğiniz kelime palindrom değildir.");
+                        }
+                    }
+                }
+                else
+                    Console.WriteLine("Girdiğiniz kelimenin en az 3 harften oluştuğuna emin olunuz.");
+            } while (kelime != "q");
+
         }
     }
 }
